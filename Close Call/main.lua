13d0f9856b0bff9922bd41f597f910311be1dbff
Mips96 <57763469+Mips96@users.noreply.github.com>
2022-06-05 18:49:49 -0400

    Close Call fix
    Close Call:
    - Added proper multiplayer support
    - Added check for Mausoleum door (Close Call is ignored if this is activated)

--- Close Call/main.lua
+++ Close Call/main.lua
@@ -2,11 +2,12 @@ CloseCall = RegisterMod("Close Call", 1)
 
 CloseCall.COLLECTIBLE_CLOSE_CALL = Isaac.GetItemIdByName("Close Call")
 
-local player
+local players = {}
+local playerNum = 0
+
 local currFrame = 0
 
 function CloseCall:onStart()
-	player = Isaac.GetPlayer(0)
 	currFrame = 0
 
 	-- External Item Description
@@ -14,6 +15,8 @@ function CloseCall:onStart()
 		__eidItemDescriptions = {}
 	end
 	__eidItemDescriptions[CloseCall.COLLECTIBLE_CLOSE_CALL] = "If your total health is 1.5 hearts or less, there is a 1/3 chance you will avoid damage#Stacks multiplicatively"
+
+	players = getPlayers()
 end
 
 function CloseCall:onUpdate()
@@ -30,25 +33,62 @@ function hasBit(var, bit)
 	return var % (bit + bit) >= bit
 end
 
-local cc_lastSafeFrame
+local cc_lastSafeFrame = {0, 0, 0, 0, 0, 0, 0, 0}
 local cc_safeColor = Color(1, 0.898, 0.396, 1, 0, 0, 0)
 local cc_numSafeFrames = 30
 
 function CloseCall:cc_onStart()
-	cc_lastSafeFrame = 0
+	cc_lastSafeFrame = {0, 0, 0, 0, 0, 0, 0, 0}
+end
+
+function damageIsFake(damageFlag)
+	return hasBit(damageFlag, DamageFlag.DAMAGE_FAKE)
+end
+
+function damageIsFromSacrificeSpikes(damageFlag)
+	return (hasBit(damageFlag, DamageFlag.DAMAGE_SPIKES) and Game():GetRoom():GetType() == RoomType.ROOM_SACRIFICE)
+end
+
+function damageIsFromMausoleumDoor(damageFlag)
+	local currStage = Game():GetLevel():GetStage()
+	return (hasBit(damageFlag, DamageFlag.DAMAGE_SPIKES)
+		and (currStage == LevelStage.STAGE2_2 or currStage == LevelStage.STAGE3_1 or currStage == LevelStage.STAGE2_1)
+		and Game():GetRoom():GetType() == RoomType.ROOM_BOSS and Game():GetRoom():IsClear())
 end
 
 function CloseCall:cc_onHit(target,damageAmount,damageFlag,damageSource,numCountdownFrames)
-	if player:HasCollectible(CloseCall.COLLECTIBLE_CLOSE_CALL) and not hasBit(damageFlag, DamageFlag.DAMAGE_FAKE) and not (hasBit(damageFlag, DamageFlag.DAMAGE_SPIKES) and Game():GetRoom():GetType() == RoomType.ROOM_SACRIFICE) then
-		if currFrame < cc_lastSafeFrame + cc_numSafeFrames then
-			return false
-		end
-		if player:GetHearts() + player:GetSoulHearts() <= 3 and math.random(1000) > (2/3)^player:GetCollectibleNum(CloseCall.COLLECTIBLE_CLOSE_CALL)*1000 then
-			cc_lastSafeFrame = currFrame
-			player:SetColor(cc_safeColor, cc_numSafeFrames, 0, true, false)
-			return false
+	if target and target.Type == EntityType.ENTITY_PLAYER then
+		playerNum = getCurrPlayerNum(target:ToPlayer())
+		if players[playerNum]:HasCollectible(CloseCall.COLLECTIBLE_CLOSE_CALL) and not damageIsFake(damageFlag) and not damageIsFromSacrificeSpikes(damageFlag) and not damageIsFromMausoleumDoor(damageFlag) then
+			if currFrame < cc_lastSafeFrame[playerNum] + cc_numSafeFrames then
+				return false
+			end
+			if players[playerNum]:GetHearts() + players[playerNum]:GetSoulHearts() <= 3 and math.random(1000) > (2/3)^players[playerNum]:GetCollectibleNum(CloseCall.COLLECTIBLE_CLOSE_CALL)*1000 then
+				cc_lastSafeFrame[playerNum] = currFrame
+				players[playerNum]:SetColor(cc_safeColor, cc_numSafeFrames, 0, true, false)
+				return false
+			end
 		end
 	end
+end
+
+function getPlayers()
+	local p = {}
+	for i = 0, Game():GetNumPlayers() do
+		if Isaac.GetPlayer(i) ~= nil then
+			table.insert(p, Isaac.GetPlayer(i))
+		end
+	end
+	return p
+end
+
+function getCurrPlayerNum(player)
+	for i = 1, #players do
+		if player:GetPlayerType() == players[i]:GetPlayerType() then
+			return i
+		end
+	end
+	return -1
 end
 
 CloseCall:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, CloseCall.cc_onStart)
