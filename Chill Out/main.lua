798a4865a11628ea80f84db863af558742cfc683
Mips96 <57763469+Mips96@users.noreply.github.com>
2022-06-05 20:54:35 -0400

    Chill Out fix + optimization
    Chill Out:
    - Fixed PostPlayerInit bug
    - Added proper multiplayer support
    - Optimized OnRender() function

--- Chill Out/main.lua
+++ Chill Out/main.lua
@@ -7,13 +7,21 @@ local json = require("json")
 
 local alreadyPlayedOnceOnBoot = false -- for Mod Config Menu; makes it so that the option is only added once per game boot
 
-local player
+local players = {}
 local currFrame = 0
 
-function ChillOut:onStart()
-	GameState = json.decode(ChillOut:LoadData())
+local co_numSafeFrames = 2 * 30
+local co_numRenderFrames1 = 2 * 18 -- number of frames that the ! will stay solid
+local co_numRenderFrames2 = co_numSafeFrames - co_numRenderFrames1 -- number of frames that the ! will fade out before the Chill Out effect expires
 
-	player = Isaac.GetPlayer(0)
+function ChillOut:onStart()
+	if ChillOut:HasData() then
+		GameState = json.decode(ChillOut:LoadData())
+	else
+		GameState = {}
+	end
+
+	players = getPlayers()
 	currFrame = 0
 
 	-- External Item Description
@@ -41,6 +49,9 @@ function ChillOut:onStart()
 				Default = 2.0,
 				OnChange = function(currentNum)
 					GameState.co_invincibilityTime = currentNum
+					co_numSafeFrames = GameState.co_invincibilityTime * 30
+					co_numRenderFrames1 = GameState.co_invincibilityTime * 18
+					co_numRenderFrames2 = co_numSafeFrames - co_numRenderFrames1
 					ChillOut:onExit()
 				end,
 				Info = {
@@ -103,20 +114,28 @@ function hasBit(var, bit)
 	return var % (bit + bit) >= bit
 end
 
+function damageIsFromMausoleumDoor(damageFlag)
+	local currStage = Game():GetLevel():GetStage()
+	return (hasBit(damageFlag, DamageFlag.DAMAGE_SPIKES)
+		and (currStage == LevelStage.STAGE2_2 or currStage == LevelStage.STAGE3_1 or currStage == LevelStage.STAGE2_1)
+		and Game():GetRoom():GetType() == RoomType.ROOM_BOSS and Game():GetRoom():IsClear())
+end
+
 local co_roomEnterFrame
-local co_canRenderText = false
+local co_canRenderText = {false, false, false, false, false, false, false, false}
 
 function ChillOut:co_onRender()
 	if GameState.co_showIcon then
-		local co_numSafeFrames = GameState.co_invincibilityTime * 30
-		local co_numRenderFrames1 = GameState.co_invincibilityTime * 18 -- number of frames that the ! will stay solid
-		local co_numRenderFrames2 = co_numSafeFrames - co_numRenderFrames1 -- number of frames that the ! will fade out before the Chill Out effect expires
-		if co_canRenderText and currFrame <= co_roomEnterFrame + co_numSafeFrames and currFrame ~= co_roomEnterFrame then
-			local playerPos = Game():GetRoom():WorldToScreenPosition(player.Position)
-			if currFrame < co_roomEnterFrame + co_numRenderFrames1 then
-				Isaac.RenderText("!", playerPos.X-3, playerPos.Y-40, 1, 1, 1, 1)
-			else
-				Isaac.RenderText("!", playerPos.X-3, playerPos.Y-40, 1, 1, 1, 1-((currFrame-co_roomEnterFrame-co_numRenderFrames1)/co_numRenderFrames2))
+		if currFrame <= co_roomEnterFrame + co_numSafeFrames and currFrame ~= co_roomEnterFrame then
+			for playerNum=1, #co_canRenderText do
+				if co_canRenderText[playerNum] then
+					local playerPos = Game():GetRoom():WorldToScreenPosition(players[playerNum].Position)
+					if currFrame < co_roomEnterFrame + co_numRenderFrames1 then
+						Isaac.RenderText("!", playerPos.X-3, playerPos.Y-40, 1, 1, 1, 1)
+					else
+						Isaac.RenderText("!", playerPos.X-3, playerPos.Y-40, 1, 1, 1, 1-((currFrame-co_roomEnterFrame-co_numRenderFrames1)/co_numRenderFrames2))
+					end
+				end
 			end
 		end
 	end
@@ -124,15 +143,38 @@ end
 
 function ChillOut:co_onNewRoom()
 	co_roomEnterFrame = currFrame
-	co_canRenderText = player:HasCollectible(ChillOut.COLLECTIBLE_CHILL_OUT)
+	co_canRenderText = {false, false, false, false, false, false, false, false}
+	for playerNum=1,Game():GetNumPlayers() do
+		co_canRenderText[playerNum] = players[playerNum]:HasCollectible(ChillOut.COLLECTIBLE_CHILL_OUT)
+	end
 end
 
 function ChillOut:co_onHit(target,damageAmount,damageFlag,damageSource,numCountdownFrames)
-	if player:HasCollectible(ChillOut.COLLECTIBLE_CHILL_OUT) then
-		if currFrame <= co_roomEnterFrame + GameState.co_invincibilityTime * 30 and (damageSource == nil or ((damageSource.Entity == nil or damageSource.Entity.SpawnerType ~= EntityType.ENTITY_PLAYER) and (damageSource.Type ~= EntityType.ENTITY_PLAYER))) and not hasBit(damageFlag, DamageFlag.DAMAGE_RED_HEARTS) and not hasBit(damageFlag, DamageFlag.DAMAGE_FAKE) and not hasBit(damageFlag, DamageFlag.DAMAGE_CURSED_DOOR) and not hasBit(damageFlag, DamageFlag.DAMAGE_CHEST) and not hasBit(damageFlag, DamageFlag.DAMAGE_INVINCIBLE) and not hasBit(damageFlag, DamageFlag.DAMAGE_TIMER) then
-			return false
+	if target and target.Type == EntityType.ENTITY_PLAYER then
+		if target:ToPlayer():HasCollectible(ChillOut.COLLECTIBLE_CHILL_OUT) then
+			if currFrame <= co_roomEnterFrame + co_numSafeFrames
+				and (damageSource == nil or ((damageSource.Entity == nil or damageSource.Entity.SpawnerType ~= EntityType.ENTITY_PLAYER) and (damageSource.Type ~= EntityType.ENTITY_PLAYER)))
+				and not hasBit(damageFlag, DamageFlag.DAMAGE_RED_HEARTS)
+				and not hasBit(damageFlag, DamageFlag.DAMAGE_FAKE)
+				and not hasBit(damageFlag, DamageFlag.DAMAGE_CURSED_DOOR)
+				and not hasBit(damageFlag, DamageFlag.DAMAGE_CHEST)
+				and not hasBit(damageFlag, DamageFlag.DAMAGE_INVINCIBLE)
+				and not hasBit(damageFlag, DamageFlag.DAMAGE_TIMER)
+				and not damageIsFromMausoleumDoor(damageFlag) then
+				return false
+			end
 		end
 	end
+end
+
+function getPlayers()
+	local p = {}
+	for i = 0, Game():GetNumPlayers() do
+		if Isaac.GetPlayer(i) ~= nil then
+			table.insert(p, Isaac.GetPlayer(i))
+		end
+	end
+	return p
 end
 
 ChillOut:AddCallback(ModCallbacks.MC_POST_RENDER, ChillOut.co_onRender)
